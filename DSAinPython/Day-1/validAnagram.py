73fa81d4a8b3a5781d814876c703b5aa06720dd4
Akash <mishrakash1701@gmail.com>
2023-12-26 19:59:54 +0530

    bruteforce

---
+++ DSAinPython/Day-1/validAnagram.py
@@ -0,0 +1,26 @@
+# An anagram is nothing but when a string 's' consist of 't' string where characters are arranged in random order
+
+s = "cat"
+t = "nagaram"
+dict1 = {}
+dict2 = {}
+
+for ch in s:
+    j = 1
+    if ch in dict1:
+        dict1[ch] += 1
+    else:
+        dict1[ch] = 1
+for ch in t:
+    j = 1
+    if ch in dict2:
+        dict2[ch] += 1
+    else:
+        dict2[ch] = 1
+
+print(dict2)
+
+if dict1 == dict2:
+    print("true")
+else:
+    print("false")
