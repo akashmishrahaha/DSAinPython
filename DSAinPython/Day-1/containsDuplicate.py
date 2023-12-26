a0ebe0fd794b04c24ccfdb7f8d3c8d472801ea0a
Akash <mishrakash1701@gmail.com>
2023-12-26 19:44:59 +0530

    better approach added

--- DSAinPython/Day-1/containsDuplicate.py
+++ DSAinPython/Day-1/containsDuplicate.py
@@ -1,19 +1,30 @@
 nums = [2, 14, 18, 22, 22]
-# return true if any value appears twice
+# # return true if any value appears twice
 
-dict = {}
+# dict = {}
 
-for i in range(len(nums)):
-    j = 1
-    if nums[i] not in dict:
-        dict[nums[i]] = j
-    elif nums[i] in dict:
-        dict[nums[i]] = j + 1
+# for i in range(len(nums)):
+#     j = 1
+#     if nums[i] not in dict:
+#         dict[nums[i]] = j
+#     elif nums[i] in dict:
+#         dict[nums[i]] = j + 1
 
-print(dict)
-for i in dict.values():
-    if i == 2:
-        print("true")
-        break
+# print(dict)
+# for i in dict.values():
+#     if i == 2:
+#         print("true")
+#         break
+#     else:
+#         continue
+
+
+# using hashset
+
+hashset = set()
+
+for num in nums:
+    if num in hashset:
+        print("True")
     else:
-        continue
+        hashset.add(num)
